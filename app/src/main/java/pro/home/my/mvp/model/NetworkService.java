c25e18842c540fbe3168cd5244a312594d619d5e
EgorStepanov <egor.stepanov@mainsoft.org>
2018-02-22 17:42:28 +0300

    ProgressDialog.

---
+++ app/src/main/java/pro/home/my/mvp/model/NetworkService.java
@@ -0,0 +1,15 @@
+package pro.home.my.mvp.model;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import pro.home.my.app.HomeApp;
+import pro.home.my.di.AppComponent;
+
+public class NetworkService {
+    public static boolean isConnection(){
+        ConnectivityManager cm = (ConnectivityManager)HomeApp.getAppComponent().getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+}
---
+++ app/src/main/java/pro/home/my/ui/dialog/ProgressDialog.java
@@ -0,0 +1,36 @@
+package pro.home.my.ui.dialog;
+
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import pro.home.my.R;
+
+public class ProgressDialog {
+
+    private AlertDialog alertDialog;
+    private TextView messageTextView;
+
+    public ProgressDialog(Context context) {
+        View view = LayoutInflater.from(context).inflate(R.layout.dialog_progress, null);
+        messageTextView = (TextView) view.findViewById(R.id.messageTextView);
+        alertDialog = new AlertDialog.Builder(context).setView(view).create();
+    }
+
+    public void show(){
+        alertDialog.show();
+    }
+
+    public void hide(){
+        alertDialog.dismiss();
+    }
+
+    public void setMessage(String message){
+        messageTextView.setText(message);
+    }
+
+    public void setMessage(int messageResId){
+        messageTextView.setText(messageResId);
+    }
+}
